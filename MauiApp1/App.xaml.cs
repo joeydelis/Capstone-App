a4688b74f4a8a814d135a38df4712d454f91c7dc
Quinton Holmes <quintonrholmes30@gmail.com>
2025-02-11 18:39:22 -0500

    dark mode DARK MODE DARK MODE and indicator

--- MauiApp1/App.xaml.cs
+++ MauiApp1/App.xaml.cs
@@ -5,11 +5,23 @@
         public App()
         {
             InitializeComponent();
+            ApplyTheme();
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
         {
             return new Window(new AppShell());
+        }
+        private void ApplyTheme()
+        {
+            if (AppInfo.RequestedTheme == AppTheme.Dark)
+            {
+                Current.Resources["PageStyle"] = Current.Resources["DarkPageStyle"];
+            }
+            else
+            {
+                Current.Resources["PageStyle"] = Current.Resources["LightPageStyle"];
+            }
         }
     }
 }
