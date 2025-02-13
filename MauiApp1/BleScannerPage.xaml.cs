a4fe5902cdca9447ddb3122aa4394117b0a3bf18
ChanningThornton <channing@ironpulley.com>
2025-02-12 22:52:27 -0500

    Basic BLE scanner and connect/disconnect

---
+++ MauiApp1/BleScannerPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1
+{
+    public partial class BleScannerPage : ContentPage
+    {
+        private readonly BleViewModel viewModel = new();
+
+        public BleScannerPage()
+        {
+            InitializeComponent();
+            BindingContext = viewModel;
+        }
+
+    }
+
+}
---
+++ MauiApp1/DeviceViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Plugin.BLE.Abstractions.Contracts;
+
+namespace MauiApp1
+{
+    public class DeviceViewModel : INotifyPropertyChanged
+    {
+        public IDevice Device { get; }
+        private bool isConnected;
+
+        public string Name => Device.Name;
+        public bool IsConnected
+        {
+            get => isConnected;
+            set
+            {
+                if (isConnected != value)
+                {
+                    isConnected = value;
+                    OnPropertyChanged(nameof(IsConnected));
+                    OnPropertyChanged(nameof(ButtonText));
+                }
+            }
+        }
+
+        public string ButtonText => IsConnected ? "Disconnect" : "Connect";
+
+        public DeviceViewModel(IDevice device)
+        {
+            Device = device;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+}
