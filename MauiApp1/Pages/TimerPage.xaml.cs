c43885c3cf7a7aebcec3db521d89fc186f96e7e0
Quinton Holmes <quintonrholmes30@gmail.com>
2025-02-13 14:43:01 -0500

    Added timer

--- MauiApp1/Pages/TimerPage.xaml.cs
+++ MauiApp1/Pages/TimerPage.xaml.cs
@@ -5,5 +5,39 @@ public partial class TimerPage : ContentPage
 	public TimerPage()
 	{
 		InitializeComponent();
+        InitializePickers();
 	}
+    private void InitializePickers()
+    {
+        // Populate the Minutes Picker
+        List<string> minutes = new List<string>();
+        for (int i = 0; i < 60; i++)
+        {
+            minutes.Add(i.ToString("D2"));
+        }
+        MinutesPicker.ItemsSource = minutes;
+
+        // Populate the Seconds Picker
+        List<string> seconds = new List<string>();
+        for (int i = 0; i < 60; i++)
+        {
+            seconds.Add(i.ToString("D2"));
+        }
+        SecondsPicker.ItemsSource = seconds;
+    }
+
+    private void OnSetTimerClicked(object sender, EventArgs e)
+    {
+        string selectedMinutes = MinutesPicker.SelectedItem?.ToString();
+        string selectedSeconds = SecondsPicker.SelectedItem?.ToString();
+
+        if (selectedMinutes != null && selectedSeconds != null)
+        {
+            DisplayAlert("Timer Set", $"Timer set for {selectedMinutes} minutes and {selectedSeconds} seconds", "OK");
+        }
+        else
+        {
+            DisplayAlert("Error", "Please select both minutes and seconds.", "OK");
+        }
+    }
 }
