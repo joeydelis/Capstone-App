6d3b1c6191fca37e7fca57135bc28833feb921ef
holmes301 <quintonrholmes30@gmail.com>
2025-02-16 02:15:44 -0500

    Added bluetooth support from BLE branch

--- MauiApp1/Pages/BluetoothConnectionPage.xaml.cs
+++ MauiApp1/Pages/BluetoothConnectionPage.xaml.cs
@@ -1,9 +1,12 @@
+using MauiApp1.Classes.Models;
 namespace MauiApp1.Pages;
 
 public partial class BluetoothConnectionPage : ContentPage
 {
-	public BluetoothConnectionPage()
+    private readonly BleViewModel viewModel = new();
+    public BluetoothConnectionPage()
 	{
 		InitializeComponent();
-	}
+        BindingContext = viewModel;
+    }
 }
